301be2c73f1dbb6bcc3248780f02917afe556a20
marcelpierres <marcelpierre.s@hotmail.com>
2021-01-06 17:31:44 -0500

    s

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: 'postgres://fkagwwmivrykje:d0758da71b7e84e40abee85cdbb8215650a1c74f081e9eb82ccc9ff5bf18e431@ec2-52-205-61-60.compute-1.amazonaws.com:5432/d3c1qd93d431ft?sslmode=require',
+    ssl: { rejectUnauthorized: false },
     pool: {
       min: 2,
       max: 10,
